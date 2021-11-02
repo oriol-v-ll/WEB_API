24845057faf57f7b3848a909b0cca680b5530a14
Oriol Villanova <62610722+oriol-v-ll@users.noreply.github.com>
2021-11-02 14:22:10 +0100

    Desenvolupat la funcionalitat basica de post. Falta retocar el html

--- myapi.js
+++ myapi.js
@@ -1,33 +1,64 @@
 //Fitxer en javascript utilitzant node JS per programar la API.
-
+//Autor: Oriol Villanova Llorens
+const Joi = require('joi');
 var http = require('http');
 var express = require('express');
 var app = express();
+app.use(express.json());
+//Podem agafar un json agafat que estigui en persistencia de dades, l'ideal es que es vagi actualitzant aquest fitxer.
 var inputs = require('./data.json');
 
-app.use(express['static'](__dirname ));
-
-// Express route for incoming requests for a customer name
-app.get('/inputs/:id', function(req, res) {
-res.set('Access-Control-Allow-Origin', '*');
-res.status(200).send(inputs[req.params.id]);
+app.use(express['static'](__dirname));
+/*
+*
+* PETICIONS POST A LA API
+*
+* */
+app.post('/api/enviar', function (req, res) {
+    if (!req.body.Temperatura || req.body.Temperatura > 50){
+        //400 Bad request
+        res.status(400).send("La temperatura no pot passar els 50 graus!");
+        return;
+    }
+    const dades_raspberry =     {
+        "Temperatura": req.body.Temperatura,
+        "Humitat": req.body.Humitat,
+        "Data": req.body.Data,
+        "Hora": req.body.Hora
+    };
+    inputs.push(dades_raspberry);
+    console.log(dades_raspberry);
+    console.log(inputs);
+    res.status(200).send("YES!")
 });
 
 
-
-// Express route for any other unrecognised incoming requests
-app.get('*', function(req, res) {
-res.status(404).send('Aixo no ho pots demanar, prova una altra cosa!');
+/*
+*
+* PETICIONS GET A LA API
+*
+* */
+app.get('/inputs/:id', function (req, res) {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.status(200).send(inputs[req.params.id]);
+    console.log('Dades enviades!');
+});
+app.get('*', function (req, res) {
+    res.status(404).send('Aixo no ho pots demanar, prova una altra cosa!');
 });
 
 // Express route to handle errors
-app.use(function(err, req, res, next) {
-if (req.xhr) {
-res.status(500).send('Alguna cosa ha anat malament!');
-} else {
-next(err);
-}
+app.use(function (err, req, res, next) {
+    if (req.xhr) {
+        res.status(500).send('Alguna cosa ha anat malament!');
+    } else {
+        next(err);
+    }
 });
 
-app.listen(3000);
-console.log('Servidor API en el port 3000');
+
+// PORT dinamic depenent de la maquina de hosting
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
+console.log(`Servidor API en el port ${PORT}`);
--- myclient.js
+++ myclient.js
@@ -1,9 +1,10 @@
 //Codi del client per a treballar amb la API.
+//Autor: Oriol Villanova Llorens
 
 window.onload = function () {
 var url, i, jqxhr;
-var json, obj;
-var length = 2;
+//De moment nomes mostrarem les 50 primeres entrades de temperatura
+var length = 50;
 
 
 for (i = 0; i < length ; i++) {
@@ -14,6 +15,7 @@ console.log('API response received');
 $('#input').append('<p>Temperatura: ' + data['Temperatura'] + ' Humitat: ' +
 data['pin'] + ' El dia:  ' + data['Data'] +' '+ data['Hora'] + '</p>');
 });
-length = Object.keys(jqxhr).length;
+//length = Object.keys(jqxhr).length;
+//console.log(length);
 }
 };
