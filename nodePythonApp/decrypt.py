a439ed43817c397ce70384f155e75edd1496aa9d
Oriol Villanova <62610722+oriol-v-ll@users.noreply.github.com>
2022-01-07 09:19:29 +0100

    Final

--- nodePythonApp/decrypt.py
+++ nodePythonApp/decrypt.py
@@ -3,8 +3,6 @@ import pythonencrypt as pe
 
 key = 'TFG'
 input = sys.stdin.readline()
-#print("Python:")
 decrypt = pe._decrypt(key,input)
-#print(decrypt)
 sys.stdout.write(decrypt)
 sys.stdout.flush(decrypt)
